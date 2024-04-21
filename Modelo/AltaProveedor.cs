294795bb7bb9b8d281eb2fd00dfc2e63d34e5977
mauroacortes <mauroacortes@yahoo.com.ar>
2024-04-21 13:34:07 -0300

    Agregue el wbs de provedor

---
+++ Modelo/AltaProveedor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class AltaProveedor
+    {
+        String _idProveedor;
+        String _idProducto;
+        string _nombre;
+        String _apellido;
+        String _email;
+        long _CUIT;
+        String _host;
+
+        public AltaProveedor(string idProveedor, string idProducto, string nombre, string apellido, string email, long CUIT, string host)
+        {
+            _idProveedor = idProveedor;
+            _idProducto = idProducto;
+            _nombre = nombre;
+            _apellido = apellido;
+            _email = email;
+            _CUIT = CUIT;
+            _host = host;
+        }
+
+        public string IdUsuario { get => _idProveedor; set => _idProveedor = value; }
+        public string Nombre { get => _nombre; set => _nombre = value; }
+        public string Apellido { get => _apellido; set => _apellido = value; }
+        public string Email { get => _email; set => _email = value; }
+        public long CUIT { get => _CUIT; set => _CUIT = value; }
+        public string Host { get => _host; set => _host = value; }
+    }
+}
---
+++ Persistencia/ProveedorService.cs
@@ -0,0 +1,122 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo;
+using Persistencia.utils;
+
+namespace Persistencia
+{
+    public class ProveedorService
+    {
+        public List<Proveedor> getProveedor()
+        {
+            String path = "/api/Proveedor/TraerProveedores";
+            List<Proveedor> proveedores = new List<Proveedor>();
+            try
+            {
+                HttpResponseMessage response = WebHelper.Get(path);
+                if (response.IsSuccessStatusCode)
+                {
+                    var contentStream = response.Content.ReadAsStringAsync().Result;
+                    List<Proveedor> listadoProveedores = JsonConvert.DeserializeObject<List<Proveedor>>(contentStream);
+                    return listadoProveedores;
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+            return proveedores;
+
+        }
+
+        public void ModificarProveedor(Guid idProveedor, String direccion, String telefono, String email)
+        {
+            String path = "/api/Proveedor/ModificarProveedor";
+            Dictionary<string, string> map = new Dictionary<string, string>();
+            map.Add("id", idProveedor.ToString());
+            map.Add("direccion", direccion);
+            map.Add("telefono", telefono);
+            map.Add("email", email);
+
+            var jsonRequest = JsonConvert.SerializeObject(map);
+
+            try
+            {
+                HttpResponseMessage response = WebHelper.Patch(path, jsonRequest);
+                if (response.IsSuccessStatusCode)
+                {
+                    var reader = new StreamReader(response.Content.ReadAsStreamAsync().Result);
+                    string respuesta = reader.ReadToEnd();
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+        }
+
+        public void AgregarProveedor(AltaProveedor altaProveedor)
+        {
+            String path = "/api/Proveedor/AgregarProveedor";
+
+            var jsonRequest = JsonConvert.SerializeObject(altaProveedor);
+
+            try
+            {
+                HttpResponseMessage response = WebHelper.Post(path, jsonRequest);
+                if (response.IsSuccessStatusCode)
+                {
+                    var reader = new StreamReader(response.Content.ReadAsStreamAsync().Result);
+                    string respuesta = reader.ReadToEnd();
+                }
+                else
+                {
+                    var reader = new StreamReader(response.Content.ReadAsStreamAsync().Result);
+                    string respuesta = reader.ReadToEnd();
+                    Console.WriteLine($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+        }
+
+        public void BorrarProveedor(Guid idProveedor)
+        {
+            String path = "/api/Cliente/BajaProveedor?id=" + idProveedor;
+
+            try
+            {
+                HttpResponseMessage response = WebHelper.Delete(path);
+                if (response.IsSuccessStatusCode)
+                {
+                    var reader = new StreamReader(response.Content.ReadAsStreamAsync().Result);
+                    string respuesta = reader.ReadToEnd();
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {response.StatusCode} - {response.ReasonPhrase}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+        }
+    }
+}
