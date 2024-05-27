3b7081d52d5a9be4642d8b5a98223ccb469d5bbd
Lisandrofor <vlisandrofor@yahoo.com.ar>
2024-05-27 14:06:19 -0300

    cambios

--- Negocio/GestorDeUsuarios.cs
+++ Negocio/GestorDeUsuarios.cs
@@ -1,21 +1,48 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-////using Modelo;
-////using AccesoaDatos;
-
-//namespace Negocio
-//{
-//    public class GestorDeUsuarios
-//    {
-//        //public String Login(Loginuser login)
-//        //{
-//        //    //String idUsuario = UsuarioDatos.Login(login);
-//        //    //return idUsuario;
-//        //}
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo;
 
 
-//    }
-//}
+namespace Negocio
+{
+    public class GestorDeUsuarios
+    {
+        public void ValidarNombre(string nombre)
+        {
+            if (string.IsNullOrEmpty(nombre) || nombre.Length <= 2 || nombre.Any(char.IsDigit))
+            {
+                throw new ArgumentException("El nombre no puede estar vacío, debe tener por lo menos 2 caracteres y no puede contener números.");
+            }
+        }
+
+        public void ValidarApellido(string apellido)
+        {
+            if (string.IsNullOrEmpty(apellido) || apellido.Length < 2 || apellido.Any(char.IsDigit))
+            {
+                throw new ArgumentException("El apellido no puede estar vacío, debe tener por lo menos 2 caracteres y no puede contener números.");
+            }
+        }
+
+        public void ValidarUsername(string nombre, string apellido, string nombreUsuario)
+        {
+            if (nombreUsuario.Length < 8)
+            {
+                throw new ArgumentException("El nombre de usuario debe tener mínimo 8 caracteres.");
+            }
+            else if (nombreUsuario.Length > 15)
+            {
+                throw new ArgumentException("El nombre de usuario debe tener un máximo de 15 caracteres.");
+            }
+            else if (nombreUsuario.IndexOf(nombre, StringComparison.OrdinalIgnoreCase) >= 0 ||
+                     nombreUsuario.IndexOf(apellido, StringComparison.OrdinalIgnoreCase) >= 0)
+            {
+                throw new ArgumentException("El nombre de usuario no debe contener el nombre o el apellido.");
+            }
+        }
+
+
+    }
+}
