f161084b6fe3b2c254b5d3c926de5ca7311e8e81
Lisandrofor <vlisandrofor@yahoo.com.ar>
2024-05-27 16:30:33 -0300

    cambios

--- Negocio/GestordeUsuariosWS.cs
+++ Negocio/GestordeUsuariosWS.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using accesoadatos;
+using Modelo;
 
 namespace Negocio
 {
-    internal class GestordeUsuariosWS
+    public class GestordeUsuariosWS
     {
+        private UsuarioDatos UsuarioDatos = new UsuarioDatos();
+        private String idAdministrador = "70b37dc1-8fde-4840-be47-9ababd0ee7e5";
+
+        public void AgregarUsuario(int host,string nombre, string apellido, int dni, string direccion, string telefono, string email, DateTime fechaNacimiento,string nombreUsuario,string contraseña,string categoria,string idusuarioactual)
+        {
+            UsuarioWS usuario = new UsuarioWS(host,idusuarioactual,nombre,apellido,dni,email,direccion,telefono,categoria,nombreUsuario,contraseña);
+            UsuarioDatos.AgregarUsuario(usuario);
+
+        }
+
     }
 }
