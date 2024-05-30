b1e3ce03a9cd134c65cfbcfc7e014b3d39628b1b
Lisandrofor <vlisandrofor@yahoo.com.ar>
2024-05-30 11:52:29 -0300

    Cambios

--- Negocio/GestordeUsuariosWS.cs
+++ Negocio/GestordeUsuariosWS.cs
@@ -11,40 +11,48 @@ namespace Negocio
     public class GestordeUsuariosWS
     {
         private UsuarioDatos UsuarioDatos = new UsuarioDatos();
-        
-        private String idAdministrador = "3fa85f64-5717-4562-b3fc-2c963f66afa6";
+
+        private String idUsuario = "3fa85f64-5717-4562-b3fc-2c963f66afa6";
 
 
-        public bool AgregarUsuario(string nombre, int host, int dni, string direccion, string telefono, string apellido,
-        string email, string idUsuarioActual, string nombreUsuario,string contraseña, DateTime fechaNacimiento)
+        //public bool AgregarUsuario(string idUsuario,string nombre, int host, int dni, string direccion, string telefono, string apellido,
+        //string email, string nombreUsuario,string contraseña, DateTime fechaNacimiento)
+        //{
+
+        //    // Crear un objeto usuarioWS
+        //    var nuevoUsuarioWS = new UsuarioWS
+        //    {
+                
+        //        idUsuario = idUsuario,
+        //        nombre = nombre,
+        //        apellido = apellido,
+        //        dni = dni,
+        //        direccion = direccion,
+        //        telefono = telefono,
+        //        email = email,
+        //        nombreUsuario = nombreUsuario,
+        //        contraseña = contraseña,
+        //        fechaNacimiento = fechaNacimiento,
+        //        host = host //pasa segun opcion menu
+        //    };
+
+        //    try
+        //    {
+        //        UsuarioDatos.AgregarUsuario(nuevoUsuarioWS);
+
+        //        return true;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return false;
+        //    }
+        //}
+
+        public void agregarCliente(string nombre, string apellido, int dni, string direccion, string telefono, string email, DateTime fechaNacimiento, int hos)
         {
+            UsuarioWS RegistroWS = new UsuarioWS(idUsuario, nombre, apellido, dni, direccion, telefono, email, fechaNacimiento,1);
+            UsuarioDatos.AgregarUsuario(RegistroWS);
 
-            // Crear un objeto usuarioWS
-            var nuevoUsuarioWS = new UsuarioWS
-            {
-                idUsuario = idUsuarioActual,
-                host = host, //pasa segun opcion menu
-                nombre = nombre,
-                apellido = apellido,
-                dni = dni,
-                direccion = direccion,
-                telefono = telefono,
-                email = email,
-                fechaNacimiento = fechaNacimiento,
-                nombreUsuario = nombreUsuario,
-                contraseña = contraseña
-            };
-
-            try
-            {
-                UsuarioDatos.AgregarUsuario(nuevoUsuarioWS);
-
-                return true;
-            }
-            catch (Exception ex)
-            {
-                return false;
-            }
         }
 
 
