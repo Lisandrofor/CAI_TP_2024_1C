f44f1436e5dc588f47f2e83d3df99ba2e9523936
Lisandrofor <vlisandrofor@yahoo.com.ar>
2024-05-29 23:56:06 -0300

    cambios

--- Negocio/GestordeUsuariosWS.cs
+++ Negocio/GestordeUsuariosWS.cs
@@ -15,15 +15,39 @@ namespace Negocio
         private String idAdministrador = "3fa85f64-5717-4562-b3fc-2c963f66afa6";
 
 
-
-
-        public void AgregarUsuario(string nombre, int host, int dni, string direccion,string categoria, string telefono, string apellido,
-       string email, string idUsuarioActual, string nombreUsuario, string contraseña, DateTime fechaNacimiento)
+        public bool AgregarUsuario(string nombre, int host, int dni, string direccion, string telefono, string apellido,
+        string email, string idUsuarioActual, string nombreUsuario,string contraseña, DateTime fechaNacimiento)
         {
-            UsuarioWS registroUsuario = new UsuarioWS(idAdministrador, nombre, apellido, dni, direccion, telefono, email,categoria,nombreUsuario,contraseña,fechaNacimiento, "test");
-            UsuarioDatos.AgregarUsuario(registroUsuario);
 
+            // Crear un objeto usuarioWS
+            var nuevoUsuarioWS = new UsuarioWS
+            {
+                idUsuario = "0cdbc5a5-69d9-4ab8-8cb3-9932ce33f54a",
+                host = host, //pasa segun opcion menu
+                nombre = nombre,
+                apellido = apellido,
+                dni = dni,
+                direccion = direccion,
+                telefono = telefono,
+                email = email,
+                fechaNacimiento = fechaNacimiento,
+                nombreUsuario = nombreUsuario,
+                contraseña = contraseña
+            };
+
+            try
+            {
+                UsuarioDatos.AgregarUsuario(nuevoUsuarioWS);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
         }
+
+
 
     }
 }
