2bf4c80d3a087e9b415385deb624530e7150d0dd
Lisandrofor <vlisandrofor@yahoo.com.ar>
2024-06-02 11:56:08 -0300

    cambios

--- Presentacion/RegistroUsuarios.cs
+++ Presentacion/RegistroUsuarios.cs
@@ -29,8 +29,8 @@ namespace Presentacion
         private void btnRegistrarUsuario_Click(object sender, EventArgs e)
         {
 
-            string host;
-            string idUsuario;
+            
+            
             string nombre = tbNombre.Text;
             string apellido = tbApellido.Text;
             int dni = int.Parse(tbDNI.Text);
