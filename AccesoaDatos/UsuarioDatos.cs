1089df19bae6e620267d74cea960f2cf0282ca52
Lisandrofor <vlisandrofor@yahoo.com.ar>
2024-06-08 17:25:30 -0300

    cambios

--- AccesoaDatos/UsuarioDatos.cs
+++ AccesoaDatos/UsuarioDatos.cs
@@ -13,122 +13,100 @@ using System.Net.Http;
 
 namespace accesoadatos
 {
-    public class UsuarioDatos
+    public static class UsuarioDatos
     {
-        public void AgregarUsuario(UsuarioWS usuario)
+        static String usuarioadmin = "70b37dc1-8fde-4840-be47-9ababd0ee7e5";
+
+        public static void AgregarUsuario(UsuarioWS usuario)
         {
-            String path = "/api/Usuario/AgregarUsuario";
-
             var jsonRequest = JsonConvert.SerializeObject(usuario);
+            HttpResponseMessage response = WebHelper.Post("Usuario/AgregarUsuario", jsonRequest);
 
-            try
+            if (!response.IsSuccessStatusCode)
             {
-                HttpResponseMessage response = WebHelper.Post(path, jsonRequest);
-                if (response.IsSuccessStatusCode)
-                {
-                    var reader = new StreamReader(response.Content.ReadAsStreamAsync().Result);
-                    string respuesta = reader.ReadToEnd();
-                }
-                else
-                {
-                    var reader = new StreamReader(response.Content.ReadAsStreamAsync().Result);
-                    string respuesta = reader.ReadToEnd();
-                    Console.WriteLine($"Error: {response.StatusCode} - {response.ReasonPhrase}");
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Exception: {ex.Message}");
+                throw new Exception("Verifique los datos ingresados. Error al agregar usuario.");
             }
         }
 
 
-
-
-
-        //post para agregar usuario
-
-        //public static void agregarusuario(UsuarioWS usuario)
-        //{
-        //    var jsonrequest = JsonConvert.SerializeObject(usuario);
-        //    HttpResponseMessage response = WebHelper.Post("usuario/agregarusuario", jsonrequest);
-
-        //    if (!response.IsSuccessStatusCode)
-        //    {
-        //        throw new Exception("verifique los datos ingresados. error al agregar usuario.");
-        //    }
-        //}
-
-
-        //get para traer usuarios
-        //public static List<UsuarioWS> listarusuarios()
-        //{
-
-        //    HttpResponseMessage response = WebHelper.Get("usuario/traerusuariosactivos?id=" + usuarioadmin);
-
-        //    if (!response.IsSuccessStatusCode)
-        //    {
-        //        throw new Exception("verifique los datos ingresados");
-        //    }
-        //    else
-        //    {
-        //        var contentstream = response.Content.ReadAsStringAsync().Result;
-        //        List<UsuarioWS> listadousuarios = JsonConvert.DeserializeObject<List<UsuarioWS>>(contentstream);
-        //        return listadousuarios;
-
-        //    }
-        //}
-
-
-        //patch cambiar contraseña
-
-        //public static string cambiarcontraseña(string nombreusuario, string contraseña, string contraseñanueva)
-        //{
-        //    Dictionary<string, string> map = new Dictionary<string, string>();
-        //    map.Add("nombreusuario", nombreusuario);
-        //    map.Add("contraseña", contraseña);
-        //    map.Add("contraseñanueva", contraseñanueva);
-
-        //    var jsonrequest = JsonConvert.SerializeObject(map);
-
-        //    HttpResponseMessage response = WebHelper.Patch("usuario/cambiarcontraseña", jsonrequest);
-
-        //    if (!response.IsSuccessStatusCode)
-        //    {
-        //        throw new Exception("verifique los datos ingresados");
-        //    }
-
-        //    var reader = new StreamReader(response.Content.ReadAsStreamAsync());
-
-        //    string respuesta = reader.ReadToEnd();
-
-        //    return respuesta;
-        //}
-
-
-        //delete para baja de usuario
-
-        //public static void borrarusuario(string idusuario, string idusuariomaster)
-        //{
-        //    Dictionary<string, string> map = new Dictionary<string, string>();
-        //    map.Add("id", idusuario);
-        //    map.Add("idusuario", idusuariomaster);
-
-        //    var jsonrequest = JsonConvert.SerializeObject(map);
-
-        //    HttpResponseMessage response = WebHelper.DeleteConBody("usuario/bajausuario", jsonrequest);
-
-        //    if (!response.IsSuccessStatusCode)
-        //    {
-        //        throw new Exception("verifique los datos ingresados");
-        //    }
-
-        //}
-
-
-
-        
-
-
     }
+
+
+
+
+
+
+
+
+    //get para traer usuarios
+    public static List<UsuarioWS> listarusuarios()
+    {
+
+        HttpResponseMessage response = WebHelper.Get("usuario/traerusuariosactivos?id=" + usuarioadmin);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception("verifique los datos ingresados");
+        }
+        else
+        {
+            var contentstream = response.Content.ReadAsStringAsync().Result;
+            List<UsuarioWS> listadousuarios = JsonConvert.DeserializeObject<List<UsuarioWS>>(contentstream);
+            return listadousuarios;
+
+        }
+    }
+
+
+    //patch cambiar contraseña
+
+    //public static string cambiarcontraseña(string nombreusuario, string contraseña, string contraseñanueva)
+    //{
+    //    Dictionary<string, string> map = new Dictionary<string, string>();
+    //    map.Add("nombreusuario", nombreusuario);
+    //    map.Add("contraseña", contraseña);
+    //    map.Add("contraseñanueva", contraseñanueva);
+
+    //    var jsonrequest = JsonConvert.SerializeObject(map);
+
+    //    HttpResponseMessage response = WebHelper.Patch("usuario/cambiarcontraseña", jsonrequest);
+
+    //    if (!response.IsSuccessStatusCode)
+    //    {
+    //        throw new Exception("verifique los datos ingresados");
+    //    }
+
+    //    var reader = new StreamReader(response.Content.ReadAsStreamAsync());
+
+    //    string respuesta = reader.ReadToEnd();
+
+    //    return respuesta;
+    //}
+
+
+    //delete para baja de usuario
+
+    //public static void borrarusuario(string idusuario, string idusuariomaster)
+    //{
+    //    Dictionary<string, string> map = new Dictionary<string, string>();
+    //    map.Add("id", idusuario);
+    //    map.Add("idusuario", idusuariomaster);
+
+    //    var jsonrequest = JsonConvert.SerializeObject(map);
+
+    //    HttpResponseMessage response = WebHelper.DeleteConBody("usuario/bajausuario", jsonrequest);
+
+    //    if (!response.IsSuccessStatusCode)
+    //    {
+    //        throw new Exception("verifique los datos ingresados");
+    //    }
+
+    //}
+
+
+
+
+
+
+}
 }
